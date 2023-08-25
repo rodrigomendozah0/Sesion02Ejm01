181dbed16bc69d8c7750640cc2ffcf98b90db307
Estudiantes <Estudiantes@ULELAB321>
2023-08-25 17:29:38 -0500

    Agregar archivos de proyecto.

---
+++ 12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Ingresa tu edad: "); 
+            edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Usted es mayor de edad");
+            }
+            else 
+            {
+                Console.WriteLine("Usted es menor de edad");
+            }
+        }
+    }
+}
